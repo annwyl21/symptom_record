ae50818aad57628812b58e77be9de5df332a74fd
Ellen Houghton <ellen.a.ash@gmail.com>
2023-06-05 10:52:34 +0100

    update form code

--- application/routes.py
+++ application/routes.py
@@ -1,35 +1,33 @@
 from flask import render_template, request
 from application import app
 from application.forms import LoginForm, RecordForm
-import os
+#import os
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title='Home')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = ""
     form = LoginForm()
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        if password == os.environ.get('symptom_record'):
-            return render_template('record.html')
-        else:
+        password = form.password.data
+        if password != 'test':
             error = "Please enter username and password"
+        else:
+            return render_template('record.html')
     else:
-        return render_template('login.html')
+        return render_template('login.html', form=form, error=error)
 
 @app.route('/record', methods=['GET', 'POST'])
 def record():
-    error = ""
     form = RecordForm()
     if request.method == 'POST':
-        symptoms = request.form['symptoms']
+        symptoms = form.symptoms.data
         if symptoms:
             return render_template('success.html')
-    return render_template('record.html')
+    return render_template('record.html', form=form)
 
 @app.route('/display_record.html')
 def display_record():
