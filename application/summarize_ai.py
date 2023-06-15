6bb7bee033449e4352d2aa84f213c55bd2d9bc3d
Ellen Houghton <ellen.a.ash@gmail.com>
2023-06-15 16:01:56 +0100

    add api connection to ai, json response received and best example of prompt following playground testing

--- application/summarize_ai.py
+++ application/summarize_ai.py
@@ -17,12 +17,24 @@ symptom_record = """Record of Symptoms over time<br>
 
 response = openai.Completion.create(
   model="text-davinci-003",
-  prompt="Perform the following actions:\n- Summarise the text delimited by triple backticks with a focus on change over time in symptoms, eg which are worsening or improving. This summary is intended to be read by a doctor or a VET in order to make a diagnosis, an onward referral or prescribe treatment from these symptoms.\n- First, find out relevant symptoms from the information provided.\n- Then, summarise the information based on the relevant symptoms.\n- Use at most 3 sentences consisting of no more than 10 words.\n- Identify the status of symptoms, are they improving or worsening?\n- Identify the sentiment of the text, eg is it positive or negative\n- Separate your answers with line breaks.\nReread your summary before you return it to me and check that all the symptoms described do exist in the original text provided.\nUse the following format:\nSummary: <summary>\nSentiment: <sentiment>\nStatus: <status>\nTime period: <time period>\nText:\n```" + symptom_record + "```",
-  temperature=0,
-  max_tokens=493,
+  prompt="""
+Perform the following actions:
+- Summarise the text delimited by triple backticks with a focus on change over time in symptoms, eg which are worsening or improving. This short summary is intended to be spoken by a person trying to convey their symptoms using easy to understand language.
+1. find out relevant symptoms from the information provided.
+2. Then, summarise the information based on the relevant symptoms.
+3. Identify the status of symptoms, are they improving or worsening?
+4. Separate your answers with line breaks.
+5. Reread your summary before you return it to me and check that all the symptoms described do exist in the original text provided.
+Use the following format:
+Summary: <short summary using 'I'>
+Status: <status>
+Time period: <time period in days>
+Text:```""" + symptom_record + "```",
+  temperature=1,
+  max_tokens=500,
   top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0
+  frequency_penalty=1,
+  presence_penalty=1
 )
 
 print(response)
