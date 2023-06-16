bad65616a8911a95a187c3628979640886dc9089
Ellen Houghton <ellen.a.ash@gmail.com>
2023-06-16 14:01:26 +0100

    hard coded bubble and scatterplot examples

--- application/bubbleplot.py
+++ application/bubbleplot.py
@@ -2,13 +2,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def bubbleplot():
-    x = np.random.rand(40)
-    y = np.random.rand(40)
-    z = np.random.rand(40)
+    #plt.switch_backend('Agg') # this changes where the computer generates the chart because that caused a problem on the Mac but not on windows
+
+    # x = np.random.rand(40)
+    # y = np.random.rand(40)
+    # z = np.random.rand(40)
+
+    # date and time are hard-coded for example because I do not yet have a database set-up that I can query using sql, from which I can create a pandas dataframe with results
+    # pain is hard-coded because I have not yet created the pain scale assessment
+    date = ['Jun 6', 'Jun 7', 'Jun 8', 'Jun 9', 'Jun 10', 'Jun 11', 'Jun 14', 'Jun 14', 'Jun 15', 'Jun 16']
+    time = [10, 21, 9, 8, 12, 11, 10, 7, 6, 10]
+    pain = [10, 15, 20, 25, 30, 30, 45, 45, 50, 55]
 
     # use the scatter function
-    plt.scatter(x, y, s=z*1000, alpha=0.5)
+    #plt.scatter(x, y, s=z*1000, alpha=0.5) where alpha is the transparency of the bubbles, to handle overlapping bubbles
+    plt.scatter(date, time, s=pain, alpha=0.5)
 
-    # show the graph
+    plt.xlabel('Date')
+    plt.ylabel('Time of Day')
+    plt.title('My Symptom Record')
     plt.savefig('application/static/images/bubbleplot.png')
     #plt.show()
+
+if __name__ == '__main__':
+    bubbleplot()
--- application/scatterplot.py
+++ application/scatterplot.py
@@ -1,13 +1,19 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
 def scatterplot():
-    price = [2.50, 1.23, 4.02, 3.25, 5.00, 4.40]
-    sales_per_day = [34, 62, 49, 22, 13, 19]
+    #plt.switch_backend('Agg') # this changes where the computer generates the chart because that caused a problem on the Mac but not on windows
 
-    plt.scatter(price, sales_per_day)
+    # date and time are hard-coded for example because I do not yet have a database set-up that I can query using sql, from which I can create a pandas dataframe with results
+    date = ['Jun 6', 'Jun 7', 'Jun 8', 'Jun 9', 'Jun 10', 'Jun 11', 'Jun 14', 'Jun 14', 'Jun 15', 'Jun 16']
+    time = [10, 21, 9, 8, 12, 11, 10, 7, 6, 10]
+
+    plt.scatter(date, time, marker='x', color='red', s=40)
+    plt.xlabel('Date')
+    plt.ylabel('Time of Day')
+    plt.title('My Symptom Record')
     plt.savefig('application/static/images/scatterplot.png')
     #plt.show()
 
 
-
+if __name__ == '__main__':
+    scatterplot()
