b9e9704849f31c92daa18bf57bfe7297c683a0a8
Ellen Houghton <ellen.a.ash@gmail.com>
2023-06-16 14:44:59 +0100

    add cleaned up plot code to generate pretty plots

--- application/bubbleplot.py
+++ application/bubbleplot.py
@@ -1,12 +1,9 @@
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import numpy as np
 
 def bubbleplot():
     #plt.switch_backend('Agg') # this changes where the computer generates the chart because that caused a problem on the Mac but not on windows
-
-    # x = np.random.rand(40)
-    # y = np.random.rand(40)
-    # z = np.random.rand(40)
 
     # date and time are hard-coded for example because I do not yet have a database set-up that I can query using sql, from which I can create a pandas dataframe with results
     # pain is hard-coded because I have not yet created the pain scale assessment
@@ -14,15 +11,25 @@ def bubbleplot():
     time = [10, 21, 9, 8, 12, 11, 10, 7, 6, 10]
     pain = [10, 15, 20, 25, 30, 30, 45, 45, 50, 55]
 
-    # use the scatter function
-    #plt.scatter(x, y, s=z*1000, alpha=0.5) where alpha is the transparency of the bubbles, to handle overlapping bubbles
+    plt.figure(figsize=(4, 3))
+    ax=plt.subplot()
+    plt.subplots_adjust(bottom = 0.2, left = 0.2)
     plt.scatter(date, time, s=pain, alpha=0.5)
 
-    plt.xlabel('Date')
-    plt.ylabel('Time of Day')
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1)) 
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(4))
+
+    plt.xticks(fontsize=6)
+    plt.xlabel('Date', fontsize=8)
+    plt.ylabel('Time of Day', fontsize=8, )
     plt.title('My Symptom Record')
     plt.savefig('application/static/images/bubbleplot.png')
-    #plt.show()
+    plt.show()
 
 if __name__ == '__main__':
     bubbleplot()
+
+# need to add code to handle missing values
+# need to add code to handle too many dates along y-axis
+
+# can I get the ai to generate the bubbleplot?
--- application/scatterplot.py
+++ application/scatterplot.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 def scatterplot():
     #plt.switch_backend('Agg') # this changes where the computer generates the chart because that caused a problem on the Mac but not on windows
@@ -7,13 +8,27 @@ def scatterplot():
     date = ['Jun 6', 'Jun 7', 'Jun 8', 'Jun 9', 'Jun 10', 'Jun 11', 'Jun 14', 'Jun 14', 'Jun 15', 'Jun 16']
     time = [10, 21, 9, 8, 12, 11, 10, 7, 6, 10]
 
+    plt.figure(figsize=(4, 3))
+    ax=plt.subplot()
+    plt.subplots_adjust(bottom = 0.2, left = 0.2)
+
+
     plt.scatter(date, time, marker='x', color='red', s=40)
-    plt.xlabel('Date')
-    plt.ylabel('Time of Day')
+
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1)) #this sets the x-axis to show every 1 day  
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(4)) #this sets the y-axis to show every 1 hour
+    
+    plt.xticks(fontsize=6)
+    plt.xlabel('Date', fontsize=8)
+    plt.ylabel('Time of Day', fontsize=8, )
     plt.title('My Symptom Record')
+    
     plt.savefig('application/static/images/scatterplot.png')
     #plt.show()
 
 
 if __name__ == '__main__':
     scatterplot()
+
+# need to add code to handle missing values
+# need to add code to handle too many dates along y-axis
