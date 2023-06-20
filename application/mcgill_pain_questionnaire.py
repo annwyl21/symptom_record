6c1ce3f1fe78172552711263b489d628f4fb71bb
Ellen Houghton <ellen.a.ash@gmail.com>
2023-06-20 17:15:17 +0100

    add pain categories to mcgill questionnaire dictionaries

--- application/mcgill_pain_questionnaire.py
+++ application/mcgill_pain_questionnaire.py
@@ -1,24 +1,44 @@
 
 mcgill_feels_like = {
+    #temporal
     1: ['flickering', 'quivering', 'pulsing', 'throbbing', 'beating', 'pounding'],
+    #spatial
     2: ['jumping', 'flashing', 'shooting'],
+    #punctate pressure
     3: ['pricking', 'boring', 'drilling', 'stabbing', 'lancinating'],
+    #incisive pressire
     4: ['sharp', 'cutting', 'lacerating'],
+    #constrictive pressure
     5: ['pinching', 'pressing', 'gnawing', 'cramping', 'crushing'],
+    #traction pressure
     6: ['tugging', 'pulling', 'wrenching'],
+    #thermal
     7: ['hot', 'boring', 'scalding', 'searing'],
+    #brightness
     8: ['tingling', 'itchy', 'smarting', 'stinging'],
+    #dullness
     9: ['dull', 'sore', 'hurting', 'aching', 'heavy'],
+    #sensory miscellaneous
     10: ['tender', 'taut', 'rasping', 'splitting'],
+    #tension
     11: ['tiring', 'exhausting'],
+    #autonomic
     12: ['sickening', 'suffocating'],
+    #fear
     13: ['fearful', 'frightful', 'terrifying'],
+    #pinishment
     14: ['punishing', 'gruelling', 'cruel', 'vicious', 'killing'],
+    #affective-evaluative-sensory: misceallaneous
     15: ['wretched', 'blinding'],
+    #evaluative
     16: ['annoying', 'troubling', 'miserable', 'intense', 'unbearable'],
+    #sensory: miscellaneous
     17: ['spreading', 'radiating', 'penetrating', 'piercing'],
+    #sensory: miscellaneous
     18: ['tight', 'numb', 'drawing', 'squeezing', 'tearing'],
+    #sensory
     19: ['cool', 'cold', 'freezing'],
+    #affective-evaluative: miscellaneous
     20: ['nagging', 'nauseating', 'agonizing', 'dreadful', 'torturing'],
 }
 
